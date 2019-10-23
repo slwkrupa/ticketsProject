7dcbe88733cf899374a04a06bb0efdc42f44e0b3
Sylwia <slw.krupa@gmail.com>
2019-10-23 17:34:05 +0200

    add new update

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
-function processingData(op, ticketNumber, ticketValue, price){
+function processingData(op, ticketNumber, ticketValue, price){ //op-operator
 
     var tkn = document.getElementById(ticketNumber).value; //tkn - ticketnumber
     var tkv = document.getElementById(ticketValue).value;  //tkv - ticketvalue
-
-    if(op=='dod'){
-        tkn++;
-        document.getElementById(ticketNumber).value = tkn;
-        tkv = parseFloat(price*tkn);
-        document.getElementById(ticketValue).value = tkv;
+  
+    if(op=='add'){
+        if(tkn<10){
+            tkn++;
+            document.getElementById(ticketNumber).value = tkn;
+            tkv = parseFloat(price*tkn);
+            document.getElementById(ticketValue).value = tkv;
+        }else{
+            button.disabled="disabled";
+        }
+    }
+    else if(op=='sub'){
+        if(tkn>=1){
+            tkn--;
+            document.getElementById(ticketNumber).value = tkn;
+            tkv = parseFloat(price*tkn);
+            document.getElementById(ticketValue).value = tkv;
+        }else{
+            button.disabled="disabled";
+        }
     }
 }
