d4be171d53ec58e7d5ad063e7d3b5ea3a48f7816
Sylwia <slw.krupa@gmail.com>
2019-10-24 17:36:09 +0200

    modifyjee

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ function processingData(op, ticketNumber, ticketValue, price){ //op-operator
     if(op=='add'){
         if(tkn<30){
             tkn++;
-            document.getElementById(ticketNumber).value = tkn;
+            document.getElementById(ticketNumber).value=tkn;
             tkv = parseFloat(price*tkn);
-            document.getElementById(ticketValue).value = tkv;
+            document.getElementById(ticketValue).value=tkv;
         }else{
             button.disabled="disabled";
         }
@@ -17,11 +17,24 @@ function processingData(op, ticketNumber, ticketValue, price){ //op-operator
     else if(op=='sub'){
         if(tkn>=1){
             tkn--;
-            document.getElementById(ticketNumber).value = tkn;
+            document.getElementById(ticketNumber).value=tkn;
             tkv = parseFloat(price*tkn);
-            document.getElementById(ticketValue).value = tkv;
+            document.getElementById(ticketValue).value=tkv;
         }else{
             button.disabled="disabled";
         }
     }
+}
+
+
+function checkBox(boxId, firstBtn, secondBtn){
+    
+    if(boxId.checked){
+        document.getElementById(firstBtn).disabled=false;
+        document.getElementById(secondBtn).disabled=false;
+    }else{
+        document.getElementById(firstBtn).disabled=true;
+        document.getElementById(secondBtn).disabled=true;
+    }
+    
 }
