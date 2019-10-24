dfeea2f996b617e947562b2ab8d77d1071e19886
Sylwia <slw.krupa@gmail.com>
2019-10-24 17:07:22 +0200

    modify

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ function processingData(op, ticketNumber, ticketValue, price){ //op-operator
 
     var tkn = document.getElementById(ticketNumber).value; //tkn - ticketnumber
     var tkv = document.getElementById(ticketValue).value;  //tkv - ticketvalue
-  
+
     if(op=='add'){
-        if(tkn<10){
+        if(tkn<30){
             tkn++;
             document.getElementById(ticketNumber).value = tkn;
             tkv = parseFloat(price*tkn);
@@ -13,6 +13,7 @@ function processingData(op, ticketNumber, ticketValue, price){ //op-operator
             button.disabled="disabled";
         }
     }
+
     else if(op=='sub'){
         if(tkn>=1){
             tkn--;
